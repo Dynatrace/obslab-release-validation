288308a44a9dafa7d25123e22f77a668f5e45513
Adam Gardner <adam.gardner@dynatrace.com>
2025-02-12 09:20:43 +1000

    add numbers to bizevents

--- docs/snippets/bizevent-enable-change.js
+++ docs/snippets/bizevent-enable-change.js
@@ -1,5 +1,5 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "enable-change"});
+  dynatrace.sendBizEvent('page_load', {"page": "8. enable-change"});
 });
 </script>
